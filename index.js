4aedab0fa2bfd8a5b43a42821711752b05fe4e12
aranajhonny <jhonnyjosearana@gmail.com>
2016-12-12 19:32:02 -0400

    edit crud operations

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 module.exports = require('./lib/db')
+// async function say(){ 
+// 	await hello()
+// 	console.log('js')
+// }
+
+// function hello(){
+// 	setTimeout(()=>{
+// 		console.log('hola')
+// 	},3000)
+// }
+// say()
